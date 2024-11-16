6ca372b995fb883daf79ef3cb8db3ff0f5dde9af
Sonu Munda <itssonuinfo@gmail.com>
2024-11-16 21:26:27 +0530

    searchbar added

--- lib/components/project_tab.dart
+++ lib/components/project_tab.dart
@@ -1,10 +1,70 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:portfoliostudent/utils/assets.dart';
+import 'package:portfoliostudent/utils/colors.dart';
 
-class ProjectTab extends StatelessWidget {
+class ProjectTab extends StatefulWidget {
   const ProjectTab({super.key});
 
   @override
+  State<ProjectTab> createState() => _ProjectTabState();
+}
+
+class _ProjectTabState extends State<ProjectTab> {
+  TextEditingController controller = TextEditingController();
+  @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      padding: const EdgeInsets.all(16),
+      child: Column(
+        children: [
+          Container(
+            padding: const EdgeInsets.all(8),
+            height: 56,
+            decoration: BoxDecoration(
+              border: Border.all(
+                color: AppColors.divider,
+                width: 1.5,
+              ),
+              borderRadius: BorderRadius.circular(12),
+            ),
+            child: Row(
+              children: [
+                Expanded(
+                  child: TextField(
+                    controller: controller,
+                    onChanged: (s) {},
+                    decoration: const InputDecoration(
+                      hintText: 'Enter text',
+                      border: InputBorder.none,
+                      hintStyle: TextStyle(color: AppColors.textLight),
+                      enabledBorder: InputBorder.none,
+                      focusedBorder: InputBorder.none,
+                      contentPadding: EdgeInsets.symmetric(horizontal: 12, vertical: 8), // Optional padding
+                    ),
+                  ),
+                ),
+                Container(
+                  width: 40,
+                  height: 40,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    color: AppColors.primary,
+                  ),
+                  child: IconButton(
+                      padding: EdgeInsets.zero,
+                      onPressed: () {},
+                      icon: SvgPicture.asset(
+                        AssetIcons.search,
+                        height: 18,
+                        width: 18,
+                      )),
+                )
+              ],
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
--- lib/utils/assets.dart
+++ lib/utils/assets.dart
@@ -4,4 +4,9 @@ class AssetIcons {
   static const String portfolio = '$base/Portfolia.svg';
   static const String input = '$base/input.svg';
   static const String profile = '$base/Profile.svg';
+
+  static const String notification = '$base/notification.svg';
+  static const String search = '$base/search.svg';
+  static const String filter = '$base/filter.svg';
+  static const String cart = '$base/cart.svg';
 }
